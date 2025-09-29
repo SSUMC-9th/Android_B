c568e0c2938279122f81b10cda31d744e3f5fa0a
ckm1217 <rudalscho41@gmail.com>
2025-09-29 10:11:30 +0900

    theo/#2에 초기 커밋

---
+++ app/src/main/java/com/example/clonecodingflo/Song.kt
@@ -0,0 +1,10 @@
+package com.example.clonecodingflo
+
+data class Song(
+    val title : String = "",
+    val singer : String = "",
+    var isPlaying : Boolean = false,
+    var second : Int = 0,
+    var minute : Int = 0,
+    var playTime : Int = 0
+)
